538f33e2c792daa770371597ebe87da8937f7d01
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-19 17:13:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('#typing').on('keydown', function(e) {
+  $('#typing').on('keydown', function(key) {
     if (key.which=== 71) {
       alert("You pressed 'g'!")
     }
