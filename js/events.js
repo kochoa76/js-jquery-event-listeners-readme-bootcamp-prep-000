f3c899e5e6a62e936bcc192bac4fa19af2845c46
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-19 17:06:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,10 +20,12 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on('submit', function() {
-    alert("Your form is going to be submitted now")
-  })
+  $("form").on("submit", function() {
+    if ($("input:first").val() === "correct") {
+    alert("Your form is going to be submitted now.")
+  }
 }
+)
 
 $(document).ready(function(){
 
